2f67deac79000eb06e802a83a2483edb5ca55969
Tobias Gippert <t.gippert@babiel.com>
2018-01-24 09:23:15 +0100

    Initial Status

--- eureka-client/src/main/java/com/babiel/tg/sandbox/microservices/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/babiel/tg/sandbox/microservices/EurekaClientApplication.java
@@ -1,12 +1,13 @@
 package com.babiel.tg.sandbox.microservices;
 
-import com.babiel.tg.sandbox.microservices.models.MessageModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 /**
  * @author t.gippert
@@ -21,8 +22,14 @@ public class EurekaClientApplication {
   }
 
   @RequestMapping("/greeting")
-  public String geeting(@RequestParam("message") final MessageModel message) {
-    return message.getMessage();
+  public String geeting(@RequestParam("locale") final Locale locale) {
+    if (Locale.GERMAN.equals(locale)) {
+      return "Deutsches Hallo vom EurekaClient";
+    } else if (Locale.FRANCE.equals(locale)) {
+      return "French hello from EurekaClient";
+    } else {
+      return "Default hello from EurekaClient";
+    }
   }
 
 }
--- greeting-client/src/main/java/com/babiel/tg/sandbox/microservices/GreetingController.java
+++ greeting-client/src/main/java/com/babiel/tg/sandbox/microservices/GreetingController.java
@@ -1,6 +1,5 @@
 package com.babiel.tg.sandbox.microservices;
 
-import com.babiel.tg.sandbox.microservices.models.MessageModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,36 +18,20 @@ public class GreetingController {
 
   @RequestMapping("/get-greeting-german")
   public String germanGreeting(Model model) {
-    MessageModel message = getMessageForLocale(Locale.GERMAN);
-    model.addAttribute("greeting", greetingClient.greeting(message));
+    model.addAttribute("greeting", greetingClient.greeting(Locale.GERMAN));
     return "greeting-view";
   }
 
   @RequestMapping("/get-greeting-french")
   public String frenchGreeting(Model model) {
-    MessageModel message = getMessageForLocale(Locale.FRANCE);
-    model.addAttribute("greeting", greetingClient.greeting(message));
+    model.addAttribute("greeting", greetingClient.greeting(Locale.FRANCE));
     return "greeting-view";
   }
 
   @RequestMapping("/get-greeting-english")
   public String englishGreeting(Model model) {
-    MessageModel message = getMessageForLocale(Locale.ENGLISH);
-    model.addAttribute("greeting", greetingClient.greeting(message));
+    model.addAttribute("greeting", greetingClient.greeting(Locale.ENGLISH));
     return "greeting-view";
-  }
-
-  private MessageModel getMessageForLocale(final Locale locale) {
-    MessageModel model = new MessageModel();
-    if (Locale.GERMAN.equals(locale)) {
-      model.setMessage("Deutsche Message");
-    } else if (Locale.FRANCE.equals(locale)) {
-      model.setMessage("Französische Message");
-    } else if (Locale.ENGLISH.equals(locale)) {
-      model.setMessage("Englische Message");
-    }
-
-    return model;
   }
 
 }
