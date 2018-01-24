5ec310b7bc66be5197ab1402d92cd38728257e7d
Tobias Gippert <t.gippert@babiel.com>
2018-01-24 09:17:58 +0100

    First commit

--- greeting-client/src/main/java/com/babiel/tg/sandbox/microservices/GreetingClient.java
+++ greeting-client/src/main/java/com/babiel/tg/sandbox/microservices/GreetingClient.java
@@ -1,10 +1,9 @@
 package com.babiel.tg.sandbox.microservices;
 
+import com.babiel.tg.sandbox.microservices.models.MessageModel;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Locale;
 
 /**
  * @author t.gippert
@@ -13,6 +12,6 @@ import java.util.Locale;
 public interface GreetingClient {
 
   @RequestMapping("/greeting")
-  String greeting(@RequestParam("locale") Locale locale);
+  String greeting(@RequestParam("message") final MessageModel message);
 
 }
